b80ad6453a642a3532669cbd38837eefc5498672
I.Popov <iliapopov91@yandex.ru>
2026-02-18 23:28:24 +0300

    My first commit

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SearchTests {
+
+    @Test
+    void selenideSearchTest () {
+
+        //Открыть хром
+        open("https://www.google.com/");
+
+        //Принять куки
+        $("[id=L2AGLb]").pressEnter();
+
+        //Ввести в поиск selenide
+        $("[name=q]").setValue("selenide").pressEnter();
+
+        //Найти в результатах результат по URL (Валится из-за капчи)
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+
+    }
+}
